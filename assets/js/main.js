135c4e88dae1f6b5f4a975e7489dbc37b73fb898
UserIsMissing <caschrei@ucsc.edu>
2025-09-29 01:41:56 -0700

    Adding personalization changes. Need alot of work to customize and add details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 /*
-	Dopetrope by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Template uses Dopetrope by HTML5 UP
 */
 
 (function($) {
