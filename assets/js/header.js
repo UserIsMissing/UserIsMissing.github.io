8c3235f434912844fd429514bd34de9443b1f412
Cole Schreiner <caschrei@ucsc.edu>
2026-03-15 16:18:31 -0700

    changed order of "Projects" dropdown

--- assets/js/header.js
+++ assets/js/header.js
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', function() {
 							<li><a href="${isProjectPage ? '' : 'projects/'}AI_Small-Scale-UAV.html">Small-Scale UAV Neural Autopilot</a></li>
 							<li><a href="${isProjectPage ? '' : 'projects/'}wet-dry-cycler.html">Wet-Dry Cycler (RNA Replicator)</a></li>
 							<li><a href="${isProjectPage ? '' : 'projects/'}professor-piano.html">Professor Piano Glove</a></li>
-							<li><a href="${isProjectPage ? '' : 'projects/'}pvnet-dog-sensor.html">PvNet Canine Gait Analysis Sensor</a></li>
 							<li><a href="${isProjectPage ? '' : 'projects/'}mechatronics-robot.html">Mechatronics Robot</a></li>
+							<li><a href="${isProjectPage ? '' : 'projects/'}pvnet-dog-sensor.html">PvNet Canine Gait Analysis Sensor</a></li>
 						</ul>
 					</li>
 					<li class="nav-coursework"><a href="${isProjectPage ? '../' : ''}index.html#coursework">Coursework</a></li>
